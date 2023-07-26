db424173a1bc8d28dc1c1b0abd6b543160469e5f
Morteza Bakhshandeh <bcfcode@gmail.com>
2023-07-26 19:36:20 +0330

    1. AppBar And Toolbar

--- src/Test.js
+++ src/Test.js
@@ -1,38 +1,17 @@
-import React, { useCallback, useState } from "react";
-import { Typography, useAutocomplete } from "@mui/material";
-import { TextField } from "@mui/material";
+import React, { useState } from "react";
+import { AppBar, Typography, Button, Toolbar } from "@mui/material";
 
 const Test = () => {
-  const [name, setName] = useState("");
   return (
     <div>
-      <TextField
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        type="text"
-        sx={{ margin: 3 }}
-        placeholder="Name"
-        variant="outlined"
-      />
-      <TextField
-        type="email"
-        sx={{ margin: 3 }}
-        placeholder="Email"
-        variant="standard"
-      />
-      <TextField
-        type="password"
-        sx={{ margin: 3 }}
-        placeholder="Password"
-        variant="filled"
-      />
-      <TextField
-        type="number"
-        sx={{ margin: 3 }}
-        placeholder="Number"
-        variant="filled"
-      />
-      <Typography>{name}</Typography>
+      <AppBar>
+        <Toolbar>
+          <Typography>LOGO</Typography>
+          <Button variant="contained" sx={{ marginLeft: "auto" }} color="success">
+            LOGIN
+          </Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
